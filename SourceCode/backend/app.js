57008ca166cfd80e3500cd56eeb54058f6c95e9a
MrTheHill <harvey.hill@hotmail.co.uk>
2025-12-01 15:04:52 +0000

    Made the basis of the backend api

--- SourceCode/backend/app.js
+++ SourceCode/backend/app.js
@@ -7,4 +7,4 @@ const port = (process.env.port || 3000);
 app.use("/api", routes);
 
 app.listen(port, () => console.log(`Api running on port ${port}`))
-module.exports = index;
+module.exports = app;
