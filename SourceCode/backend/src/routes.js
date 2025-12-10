b5b6f390c8b0ea6424bb9dabf191bd75f2e96b01
Harvey Hill <harvey.hill@hotmail.co.uk>
2025-12-10 14:43:34 +0000

    Add new route for '/prompt' endpoint

--- SourceCode/backend/src/routes.js
+++ SourceCode/backend/src/routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require("./controller")
 
 router.get("/ping", controller.ping)
+router.get("/prompt", controller.ping)
 
 module.exports = router;
